6ed682b72ea723dcdfc77161e458b0c659bc4dd9
Ant <ant@pop-os.localdomain>
2020-10-25 23:32:05 -0400

    sidebar functional, basic styles added.

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './sidebar.scss'
 import { NavLink } from 'react-router-dom'
 import {ReactComponent as X} from '../../assets/times-solid.svg' 
+import Button from '../button/button'
 
 const Sidebar = ({ showSidebar, toggleSidebar }) => {
   return (
@@ -10,11 +11,15 @@ const Sidebar = ({ showSidebar, toggleSidebar }) => {
           <X />
         </div>
         <ul>
-          
-            <li>Test</li>
-            <li>Test</li>
-            <li>Test</li>
+            <li><NavLink to='/pricing'>Pricing</NavLink></li>
+            <li><NavLink to='/about'>About</NavLink></li>
+            <li><NavLink to='contact'>Contact</NavLink></li>
         </ul>
+        <NavLink className='schedule' to='/contact'>
+            <Button btnStyle='dark-pink'>
+              Schedule a Demo
+            </Button>
+        </NavLink>
     </div>
   )
 }
