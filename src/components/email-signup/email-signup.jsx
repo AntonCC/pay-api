f283c81b8d4ef7e4fb46470c771be654b8d5cec0
Anton <antonchetvv@gmail.com>
2020-11-11 16:37:02 -0500

    switched email-signup form handle to Formik.

--- src/components/email-signup/email-signup.jsx
+++ src/components/email-signup/email-signup.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import './email-signup.scss'
 import Button from '../button/button'
+import { Formik, Form, Field } from 'formik'
 
 const EmailSignUp = () => {
+  const handleSubmit = values => {
+    console.log(values)
+  }
+
+
   return (
     <div className='email-signup'>
-      <form>
-        <input type="email" placeholder='Enter email address' required />
-        <div className="btn-wrap">
-          <Button btnStyle='dark-pink'>
-            Schedule a Demo
-          </Button>
-        </div>
-      </form>
+      <Formik
+        initialValues={{ email: '' }}
+        validate={values => {
+          const errors = {}
+          if(!values.email) {
+            errors.email = 'Required'
+          } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Invalid Email Address'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          handleSubmit(values)
+        }}
+      >
+        <Form>
+          <input type="email" placeholder='Enter email address' required />
+          <div className="btn-wrap">
+            <Button btnStyle='dark-pink'>
+              Schedule a Demo
+            </Button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   )
 }
